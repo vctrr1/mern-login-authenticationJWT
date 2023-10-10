78fc3ee3588c7ce809d08505a1ab41665cb29c63
Victor Emanoel <victoremaanoel@outlook.com>
2023-10-10 19:00:07 -0300

    feat: userController and api routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,17 @@
 import express from "express";
 const router = express.Router();
 
-import { authUser } from "../controllers/userController.js";
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+} from "../controllers/userController.js";
 
-router.post("/auth/", authUser);
+router.post("/", authUser);
+router.post("/auth", authUser);
+router.post("/logout", logoutUser);
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
 
 export default router;
